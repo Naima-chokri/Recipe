bb89a343b2b917bdb91e4e1b9ef58aa6e8d3ac13
Naima-chokri <nematchokri@gmail.com>
2022-06-14 21:26:10 +0100

    details-styling

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import "./Details.css"
+
 const Details = () => {
    const {id} =useParams()
    const [oneRec, setOneRec] = useState({})
@@ -19,12 +21,28 @@ const Details = () => {
     getOneReceipece()
     }, 
     [])
+    console.log(oneRec)
 
   return (
     <div>
-        {loading ? <h1>Loading...</h1> :<div>
-        
-        {oneRec.recipe.label}</div>}
+        {loading ? <h1>Loading...</h1> :
+        <div> 
+          <div className="wrapper">
+            <div className="product-img">
+              <img src={oneRec.recipe.image} height={450} width={425} />
+            </div>
+            <div className="product-info">
+              <div className="product-text">
+                <h1>{oneRec.recipe.label}</h1>
+                <h2>{oneRec.recipe.cuisineType}</h2>
+                <p>{oneRec.recipe.ingredientLines}</p>
+              </div>
+              <div className="product-price-btn">
+                <button type="button">Order Ingredients</button>
+              </div>
+            </div>
+        </div>
+      </div>}
     </div>
   )
 }
